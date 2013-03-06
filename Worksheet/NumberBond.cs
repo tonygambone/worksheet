2b16d9204fa4ccd05dff97c6b134f91d50e791fd
Tony Gambone <tonygambone@gmail.com>
2013-03-06 18:57:01 -0500

    Initial commit.

---
+++ Worksheet/NumberBond.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet
+{
+    class NumberBond
+    {
+        private static Random rand = new Random();
+
+        private NumberBond() {}
+
+        public int? Sum { get; private set; }
+        public int? Addend1 { get; private set; }
+        public int? Addend2 { get; private set; }
+
+        public static IEnumerable<NumberBond> GetNumberBonds(int sum)
+        {
+            for (int a1 = 0; a1 <= sum / 2; a1++)
+                yield return new NumberBond
+                {
+                    Sum = sum,
+                    Addend1 = a1,
+                    Addend2 = sum - a1,
+                };
+        }
+
+        public static NumberBond GetRandomNumberBond(int sum)
+        {
+            var a1 = rand.Next(0, sum);
+            return new NumberBond { Sum = sum, Addend1 = a1, Addend2 = sum - a1 };
+        }
+
+        public static IEnumerable<NumberBond> GetRandomNumberBonds()
+        {
+            while (true)
+            {
+                var sum = rand.Next(0, 11);
+                yield return GetRandomNumberBond(sum);
+            }
+        }
+
+        public static IEnumerable<NumberBond> GetRandomNumberBondProblems()
+        {
+            foreach (var b in GetRandomNumberBonds())
+            {
+                switch (rand.Next(0, 3))
+                {
+                    case 0:
+                        b.Sum = null; break;
+                    case 1:
+                        b.Addend1 = null; break;
+                    case 2:
+                        b.Addend2 = null; break;
+                }
+                yield return b;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sum = Sum.HasValue ? Sum.Value.ToString() : "_";
+            var a1 = Addend1.HasValue ? Addend1.Value.ToString() : "_";
+            var a2 = Addend2.HasValue ? Addend2.Value.ToString() : "_";
+            return string.Format("{0} + {1} = {2}", 
+                a1, 
+                a2, 
+                sum);
+        }
+    }
+}
---
+++ Worksheet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // number that fit on one page when printed from Chrome
+                int problemCount = 48;
+
+                var sb = new StringBuilder();
+                var bonds = NumberBond.GetRandomNumberBondProblems().GetEnumerator();
+                for (int i = 0; i < problemCount; i++)
+                {
+                    if (bonds.MoveNext())
+                    {
+                        var b = bonds.Current;
+                        sb.AppendFormat("<div><div>{0}</div><div>{1}</div><div>{2}</div><div></div></div>\r\n",
+                            b.Sum, b.Addend1, b.Addend2);
+                    }
+                }
+
+                string content;
+                using (var reader = new StreamReader("template.html"))
+                {
+                    content = reader.ReadToEnd();                    
+                }
+                content = content.Replace("<!--#BONDS#-->", sb.ToString());
+                using (var writer = new StreamWriter("out.html"))
+                {
+                    writer.Write(content);
+                }
+                Process.Start("out.html");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+    }
+}
